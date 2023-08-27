188c23f9f3d34b674606ee3a70f86b9d3193f98c
rupam79 <rupamdhoundiyal47@gmail.com>
2023-08-27 12:58:20 +0530

    JS fundamentals

---
+++ JS fundamentals 1/script.js
@@ -0,0 +1,35 @@
+/*
+let js='good';
+if(js=='good'){
+    alert("JS is fun");
+}
+*/
+//we should use camelCase for variables
+
+//rules for variables
+/*
+        1.cannot start with a number.
+        2.cannot contain spl char.
+        3.can contain _ $.
+        4.cannot use keywords.
+        5.dont use uppercase as first letter of var.
+        6.uppercase are for constants and used fot OOP.
+        7.varibales should be descriptive.
+    */
+
+
+//DATA TYPES
+
+//1.PRIMITIVE:
+    /*
+        1.1 number (all numbers)
+        1.2 string ("" '')
+        1.3 boolean
+        1.4 undefined (int age;)
+        1.5 Null
+        1.6 symbol (unique value)
+        1.7 Big int(for large vlaues)
+
+        **JS is dynamically typed language**
+        **typeof(var)**
+    */
